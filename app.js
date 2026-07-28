02b98e9c3fb89d06ba1274b8548b11aa9daa0e12
kyclau <kyclau@gmail.com>
2026-07-28 16:40:43 +0800

    Update app.js

--- app.js
+++ app.js
@@ -535,8 +535,14 @@ function render() {
 
 // Setup event listeners
 function setupEventListeners() {
-    addWatchBtn.addEventListener('click', () => openAddModal());
-    emptyAddBtn.addEventListener('click', () => openAddModal());
+    // Add safety check for addWatchBtn
+    if (addWatchBtn) {
+        addWatchBtn.addEventListener('click', () => openAddModal());
+    }
+    
+    if (emptyAddBtn) {
+        emptyAddBtn.addEventListener('click', () => openAddModal());
+    }
     document.getElementById('closeModal').addEventListener('click', closeAddModal);
     document.getElementById('cancelBtn').addEventListener('click', closeAddModal);
     document.getElementById('closeDetail').addEventListener('click', closeDetailModal);
